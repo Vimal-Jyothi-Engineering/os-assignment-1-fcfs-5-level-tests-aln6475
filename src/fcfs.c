96678b3bfbda7fdf92dec9276750d65e32ac9012
ALANTA MARIYA BABU <alantamariya.shibi65@gmail.com>
2026-03-01 17:08:22 +0530

    Added FCFS scheduling implementation by sorting processes based on arrival time and computing waiting time, turnaround time, total WT, total TAT, average WT, and average TAT.

--- src/fcfs.c
+++ src/fcfs.c
@@ -18,10 +18,44 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
     }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].arrival > p[j].arrival) {
+                Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+    int currentTime = 0;
+    float totalWT = 0, totalTAT = 0;
 
-    // TODO: Sort by arrival time
-    // TODO: Compute waiting & turnaround time
-    // TODO: Print EXACTLY in required format
+    for (int i = 0; i < n; i++) {
+        if (currentTime < p[i].arrival)
+            currentTime = p[i].arrival;
+
+        p[i].waiting = currentTime - p[i].arrival;
+        currentTime += p[i].burst;
+        p[i].turnaround = p[i].waiting + p[i].burst;
+
+        totalWT += p[i].waiting;
+        totalTAT += p[i].turnaround;
+    }
+
+    float avgWT = totalWT / n;
+    float avgTAT = totalTAT / n;
+    printf("Waiting Time: ");
+    for (int i = 0; i < n; i++) {
+        printf("%s %d ", p[i].pid, p[i].waiting);
+    }
+
+    printf("\nTurnaround Time: ");
+    for (int i = 0; i < n; i++) {
+        printf("%s %d ", p[i].pid, p[i].turnaround);
+    }
+
+    printf("\nAverage Waiting Time: %.2f", avgWT);
+    printf("\nAverage Turnaround Time: %.2f", avgTAT);
 
     return 0;
 }
